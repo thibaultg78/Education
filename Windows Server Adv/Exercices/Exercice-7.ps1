f6a510f095ea27115dbdc3f3e6a3c07e0802ecff
thibaultg78 <thibaultg78@users.noreply.github.com>
2024-02-03 11:22:30 +0100

    adding windows server exercices

---
+++ Windows Server Adv/Exercices/Exercice-7.ps1
@@ -0,0 +1,13 @@
+﻿$allservices = Get-Service
+foreach ($item in $allservices)
+{
+    if ($item.Status -eq "Running")
+    {
+        Write-host $item.Name $item.DisplayName $item.Status -ForegroundColor Green
+    }
+
+    else
+    {
+        Write-host $item.Name $item.DisplayName $item.Status -ForegroundColor Red
+    }
+}
