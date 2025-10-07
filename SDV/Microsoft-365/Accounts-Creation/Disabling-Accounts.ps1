140a57f5a827c7b07576c5cdaa58b3d770182626
Thibault Gibard <gibard.thibault@gmail.com>
2025-10-08 00:00:10 +0200

    education added folder sdv public

---
+++ SDV/Microsoft-365/Accounts-Creation/Disabling-Accounts.ps1
@@ -0,0 +1,30 @@
+#Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+$params = @{
+	accountEnabled = $true
+}
+Update-MgUser -UserId "user01@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user02@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user03@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user04@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user05@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user06@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user07@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user08@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user09@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user10@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user11@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user12@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user13@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user14@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user15@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user16@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user17@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user18@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user19@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user20@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user21@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user22@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user23@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user24@1xyj7c.onmicrosoft.com" -BodyParameter $params
+Update-MgUser -UserId "user25@1xyj7c.onmicrosoft.com" -BodyParameter $params
