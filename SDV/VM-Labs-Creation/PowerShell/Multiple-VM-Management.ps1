09e83c0c1b8695397fb52987f4d25bf96ab2a6da
Thibault Gibard <gibard.thibault@gmail.com>
2025-10-23 19:48:31 +0200

    comit

--- SDV/VM-Labs-Creation/PowerShell/Multiple-VM-Management.ps1
+++ SDV/VM-Labs-Creation/PowerShell/Multiple-VM-Management.ps1
@@ -12,4 +12,5 @@ Get-AzVM | Where-Object { $_.Name -like "*SDV*" } | ForEach-Object {
 }
 
 # Display the VM name its power state
-Get-AzVM -Status | Where-Object { $_.Name -like "*SDV*" } | Select-Object Name, PowerState
+# Get-AzVM -Status | Where-Object { $_.Name -like "*SDV*" } | Select-Object Name, PowerState
+Get-AzVM -Status | Where-Object { $_.Name -like "*SDV*" } | Select-Object Name, ResourceGroupName, PowerState, Location
